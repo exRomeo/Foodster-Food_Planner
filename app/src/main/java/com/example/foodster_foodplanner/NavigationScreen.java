29b2343766a51ae3bcac51e0edf67ca88214c1a8
Sahar Nasser <saharnasser150@gmail.com>
2023-02-05 23:14:34 +0200

    adding navigation UI
    -adding navigation screen ui with no functionality
    -fixing icon colors
    -adding user profile icon

---
+++ app/src/main/java/com/example/foodster_foodplanner/NavigationScreen.java
@@ -0,0 +1,15 @@
+package com.example.foodster_foodplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class NavigationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_screen);
+    }
+}
