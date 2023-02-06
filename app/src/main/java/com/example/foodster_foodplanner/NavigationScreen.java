874f4627bceddf1111778a6ea9e9e6223ceb6031
Sahar Nasser <saharnasser150@gmail.com>
2023-02-06 20:12:37 +0200

    adding login screen
    adding login screen UI
    adding Home Screen cards

--- app/src/main/java/com/example/foodster_foodplanner/NavigationScreen.java
+++ app/src/main/java/com/example/foodster_foodplanner/NavigationScreen.java
@@ -3,13 +3,22 @@ package com.example.foodster_foodplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class NavigationScreen extends AppCompatActivity {
-
+      ImageButton back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_screen);
+        back = findViewById(R.id.BackIcon);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
