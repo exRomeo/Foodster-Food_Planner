17c2677babdea16a8289c3f95b3c2e4d2d753319
Ramy Ashraf <Ramy3r@hotmail.com>
2023-02-06 23:24:12 +0200

    Added Retrofit Interface

--- app/src/main/java/com/example/foodster_foodplanner/retrofitclient/RetrofitClientInterface.java
+++ app/src/main/java/com/example/foodster_foodplanner/retrofitclient/RetrofitClientInterface.java
@@ -1,11 +1,10 @@
 package com.example.foodster_foodplanner.retrofitclient;
 
 public interface RetrofitClientInterface {
+    void getRandomMeal();
 
     void searchByName(String strMeal);
 
     void listByFirstLetter(char firstLetter);
-
-    void getRandomMeal();
 }
 
