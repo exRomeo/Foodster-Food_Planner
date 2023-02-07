7d1b6abb88833bbc0a7d60735b20eefdb3e73f9a
Ramy Ashraf <ramy3r@hotmail.com>
2023-02-07 21:15:32 +0200

    merging to development branch

--- app/src/main/java/com/example/foodster_foodplanner/models/Meal.java
+++ app/src/main/java/com/example/foodster_foodplanner/models/Meal.java
@@ -1,6 +1,5 @@
 package com.example.foodster_foodplanner.models;
 
-import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 import androidx.room.TypeConverters;
@@ -15,7 +14,6 @@ import java.util.List;
 @TypeConverters({DateConverter.class})
 public class Meal {
     @PrimaryKey
-    @NonNull
     private final int idMeal;
     private final String strMeal;
     private final String strDrinkAlternate;
@@ -70,7 +68,7 @@ public class Meal {
     private final String strCreativeCommonsConfirmed;
     private final String dateModified;
     private boolean isFavorite;
-    Date date;
+    private final Date date;
 
 
     public Meal(int idMeal, String strMeal, String strDrinkAlternate, String strCategory, String strArea, String strInstructions, String strMealThumb, String strTags, String strYoutube, String strIngredient1, String strIngredient2, String strIngredient3, String strIngredient4, String strIngredient5, String strIngredient6, String strIngredient7, String strIngredient8, String strIngredient9, String strIngredient10, String strIngredient11, String strIngredient12, String strIngredient13, String strIngredient14, String strIngredient15, String strIngredient16, String strIngredient17, String strIngredient18, String strIngredient19, String strIngredient20, String strMeasure1, String strMeasure2, String strMeasure3, String strMeasure4, String strMeasure5, String strMeasure6, String strMeasure7, String strMeasure8, String strMeasure9, String strMeasure10, String strMeasure11, String strMeasure12, String strMeasure13, String strMeasure14, String strMeasure15, String strMeasure16, String strMeasure17, String strMeasure18, String strMeasure19, String strMeasure20, String strSource, String strImageSource, String strCreativeCommonsConfirmed, String dateModified, Date date) {
@@ -361,6 +359,10 @@ public class Meal {
 
     public void setFavorite(boolean favorite) {
         isFavorite = favorite;
+    }
+
+    public Date getDate() {
+        return date;
     }
 
     public List<String> getIngredientsList() {
