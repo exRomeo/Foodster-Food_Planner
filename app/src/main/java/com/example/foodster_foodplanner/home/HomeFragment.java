1d41443d99db51c95025573ffefa6ae5d60e4435
Sahar Nasser <saharnasser150@gmail.com>
2023-02-09 18:00:09 +0200

    updates to branch

--- app/src/main/java/com/example/foodster_foodplanner/home/HomeFragment.java
+++ app/src/main/java/com/example/foodster_foodplanner/home/HomeFragment.java
@@ -1,36 +1,29 @@
-package com.example.foodster_foodplanner;
+package com.example.foodster_foodplanner.home;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.foodster_foodplanner.R;
+import com.example.foodster_foodplanner.models.Meal;
+
+import java.util.ArrayList;
+
+
 public class HomeFragment extends Fragment {
 
-   RecyclerView homeRecycler;
-
+   private ViewPager2 viewPager;
+   private ArrayList<Meal> dailyTen;
+   private PageViewerAdapter adapter;
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         return fragment;
@@ -40,12 +33,15 @@ public class HomeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        viewPager = container.findViewById(R.id.viewPager);
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
---
+++ app/src/main/java/com/example/foodster_foodplanner/home/OnFavoriteIconClickListener.java
@@ -0,0 +1,7 @@
+package com.example.foodster_foodplanner.home;
+
+import com.example.foodster_foodplanner.models.Meal;
+
+public interface OnFavoriteIconClickListener{
+        void onClick(Meal meal);
+}
