bacf20575cc947e55809dd349538181caf45f5ad
Ramy Ashraf <Ramy3r@hotmail.com>
2023-02-10 22:19:07 +0200

    minor refactoring and color fixing

--- app/src/main/java/com/example/foodster_foodplanner/fragments/planner/PlannerFragment.java
+++ app/src/main/java/com/example/foodster_foodplanner/fragments/planner/PlannerFragment.java
@@ -5,11 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.foodster_foodplanner.R;
+import com.example.foodster_foodplanner.databinding.FragmentPlannerBinding;
 
 public class PlannerFragment extends Fragment {
+    FragmentPlannerBinding binding;
 
     public PlannerFragment() {
         // Required empty public constructor
@@ -29,5 +33,18 @@ public class PlannerFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_planner, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding = FragmentPlannerBinding.bind(view);
+//        binding.saturdayAdd;
+//        binding.sundayAdd;
+//        binding.mondayAdd;
+//        binding.tuesdayAdd;
+//        binding.wednesdayAdd;
+//        binding.thursdayAdd;
+//        binding.fridayAdd;
     }
 }
---
+++ app/src/main/java/com/example/foodster_foodplanner/fragments/planner/PlannerView.java
@@ -0,0 +1,7 @@
+package com.example.foodster_foodplanner.fragments.planner;
+
+import com.example.foodster_foodplanner.models.Meal;
+
+public interface PlannerView {
+    void addMealToPlan(Meal meal);
+}
