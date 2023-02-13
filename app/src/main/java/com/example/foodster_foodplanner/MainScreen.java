841d6a8533865e08b3fd5bd0bc98fb147cd22556
Sahar Nasser <saharnasser150@gmail.com>
2023-02-13 17:02:18 +0200

    fragmenting search screen

--- app/src/main/java/com/example/foodster_foodplanner/MainScreen.java
+++ app/src/main/java/com/example/foodster_foodplanner/MainScreen.java
@@ -1,6 +1,9 @@
 package com.example.foodster_foodplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,19 +11,50 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import com.example.foodster_foodplanner.fragments.meal.MealFragment;
+import com.example.foodster_foodplanner.fragments.search.MainSearchFragment;
+import com.example.foodster_foodplanner.home.HomeFragment;
+
 public class MainScreen extends AppCompatActivity {
-     ImageButton menu;
+     ImageButton menu,search,home;
      Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_main);
         menu = findViewById(R.id.menuIcon);
+        search=findViewById(R.id.searchIcon);
+        home=findViewById(R.id.homeIcon);
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 intent= new Intent(MainScreen.this,NavigationScreen.class);
                 startActivity(intent);
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new MainSearchFragment();
+                FragmentManager fragmentManager = MainScreen.this.getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.mainFragmentContainer, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = new HomeFragment();
+                FragmentManager fragmentManager = MainScreen.this.getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.mainFragmentContainer, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
             }
         });
     }
--- app/src/main/java/com/example/foodster_foodplanner/fragments/search/SpecifiedSearch.java
+++ app/src/main/java/com/example/foodster_foodplanner/fragments/search/SpecifiedSearch.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 
 import com.example.foodster_foodplanner.R;
 
-public class MainSearchFragment extends Fragment {
+public class SpecifiedSearch extends Fragment {
 
-
-    public MainSearchFragment() {
+    public SpecifiedSearch() {
         // Required empty public constructor
     }
+
 
 
     @Override
@@ -30,7 +30,7 @@ public class MainSearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_search, container, false);
+        return inflater.inflate(R.layout.fragment_specified_search, container, false);
     }
 
     @Override
