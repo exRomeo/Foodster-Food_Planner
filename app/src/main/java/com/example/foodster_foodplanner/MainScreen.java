e0d70cfa4fd7d7f39c8a425034de5e74dea2b72c
Ramy Ashraf <Ramy3r@hotmail.com>
2023-02-15 00:44:25 +0200

    working with navGraph

--- app/src/main/java/com/example/foodster_foodplanner/MainScreen.java
+++ app/src/main/java/com/example/foodster_foodplanner/MainScreen.java
@@ -6,19 +6,27 @@ import android.view.View;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.foodster_foodplanner.databinding.ScreenMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainScreen extends AppCompatActivity {
     ImageButton menu, search, home;
     Intent intent;
     ScreenMainBinding binding;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ScreenMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        navController = Navigation.findNavController(this,R.id.main_fragment_host);
+       BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+       NavigationUI.setupWithNavController(bottomNavigationView,navController);
 
         menu = findViewById(R.id.menuIcon);
 //        search = findViewById(R.id.searchIcon);
