cd4af1c1cd46aff78d7beea0916d2a54fb98e66c
Ramy Ashraf <Ramy3r@hotmail.com>
2023-02-15 04:44:13 +0200

    working with navGraph -- updated back button function to work better with navgraph

--- app/src/main/java/com/example/foodster_foodplanner/fragments/meal/MealFragment.java
+++ app/src/main/java/com/example/foodster_foodplanner/fragments/meal/MealFragment.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.bumptech.glide.Glide;
 import com.example.foodster_foodplanner.R;
@@ -54,7 +55,7 @@ public class MealFragment extends Fragment implements MealView {
         showMeal(currentMeal);
         binding.addToPlan.setOnClickListener(v -> createDialog().show());
         binding.rightBottomButton.setOnClickListener(v -> addToFavorites(currentMeal));
-        binding.backButton.setOnClickListener(v -> getParentFragmentManager().popBackStack());
+        binding.backButton.setOnClickListener(v -> NavHostFragment.findNavController(this).popBackStack());
 
     }
 
