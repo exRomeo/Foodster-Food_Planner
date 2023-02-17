cbd9ca4adcc9c52d82288a70d7b35b9cddd19d42
Ramy Ashraf <Ramy3r@hotmail.com>
2023-02-17 21:34:46 +0200

    Adding youtube player

---
+++ app/src/main/java/com/example/foodster_foodplanner/fragments/meal/YoutubeConfig.java
@@ -0,0 +1,10 @@
+package com.example.foodster_foodplanner.fragments.meal;
+
+public class YoutubeConfig {
+    private static final String YOUTUBE_API_KEY = "AIzaSyCnDfBcSje4kLHZK5Y2DB0kT0NM3sHklVo";
+    public YoutubeConfig() {
+    }
+    public static String getYoutubeApiKey(){
+        return YOUTUBE_API_KEY;
+    }
+}
